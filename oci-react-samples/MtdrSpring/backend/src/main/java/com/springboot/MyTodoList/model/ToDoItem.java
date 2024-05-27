8e85cec3b937723f7f8d38ebb1d345ab755cf1c8
Fergomar1320 <a01641228@tec.mx>
2024-05-27 15:57:29 -0600

    All models added

--- oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/ToDoItem.java
+++ oci-react-samples/MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/model/ToDoItem.java
@@ -13,62 +13,106 @@ import java.time.OffsetDateTime;
 public class ToDoItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    int ID;
-    @Column(name = "DESCRIPTION")
-    String description;
-    @Column(name = "CREATION_TS")
-    OffsetDateTime creation_ts;
-    @Column(name = "done")
-    boolean done;
+    private int item_id;
+
+    @Column(name = "ITEM_DESCRIPTION")
+    private String item_description;
+
+    @Column(name = "ITEM_CREATION_TS")
+    private OffsetDateTime item_creation_ts;
+
+    @Column(name = "ITEM_DEADLINE")
+    private OffsetDateTime item_deadline;
+
+    @Column(name = "ITEM_STATUS")
+    private String item_status;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private OracleUser user;
+
+    @ManyToOne
+    @JoinColumn(name = "SPRINT_ID", nullable = false)
+    private ToDoSprint sprint;
+
     public ToDoItem(){
 
     }
-    public ToDoItem(int ID, String description, OffsetDateTime creation_ts, boolean done) {
-        this.ID = ID;
-        this.description = description;
-        this.creation_ts = creation_ts;
-        this.done = done;
+    public ToDoItem(int item_id, String item_description, OffsetDateTime item_creation_ts, String item_status, OracleUser user, ToDoSprint sprint) {
+        this.item_id = item_id;
+        this.item_description = item_description;
+        this.item_creation_ts = item_creation_ts;
+        this.item_status = item_status;
+        this.user = user;
+        this.sprint = sprint;
     }
 
-    public int getID() {
-        return ID;
+    public int getItemId() {
+        return item_id;
     }
 
-    public void setID(int ID) {
-        this.ID = ID;
+    public String getItemDescription() {
+        return item_description;
     }
 
-    public String getDescription() {
-        return description;
+    public OffsetDateTime getItemCreationTs() {
+        return item_creation_ts;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public OffsetDateTime getItemDeadline() {
+        return item_deadline;
     }
 
-    public OffsetDateTime getCreation_ts() {
-        return creation_ts;
+    public String getItemStatus() {
+        return item_status;
     }
 
-    public void setCreation_ts(OffsetDateTime creation_ts) {
-        this.creation_ts = creation_ts;
+    public OracleUser getUser() {
+        return user;
     }
 
-    public boolean isDone() {
-        return done;
+    public ToDoSprint getSprint() {
+        return sprint;
     }
 
-    public void setDone(boolean done) {
-        this.done = done;
+    public void setItemId(int item_id) {
+        this.item_id = item_id;
+    }
+
+    public void setItemDescription(String item_description) {
+        this.item_description = item_description;
+    }
+
+    public void setItemCreationTs(OffsetDateTime item_creation_ts) {
+        this.item_creation_ts = item_creation_ts;
+    }
+
+    public void setItemDeadline(OffsetDateTime item_deadline) {
+        this.item_deadline = item_deadline;
+    }
+
+    public void setItemStatus(String item_status) {
+        this.item_status = item_status;
+    }
+
+    public void setUser(OracleUser user) {
+        this.user = user;
+    }
+
+    public void setSprint(ToDoSprint sprint) {
+        this.sprint = sprint;
     }
 
     @Override
     public String toString() {
         return "ToDoItem{" +
-                "ID=" + ID +
-                ", description='" + description + '\'' +
-                ", creation_ts=" + creation_ts +
-                ", done=" + done +
+                "item_id=" + item_id +
+                ", item_description='" + item_description + '\'' +
+                ", item_creation_ts=" + item_creation_ts +
+                ", item_deadline=" + item_deadline +
+                ", item_status='" + item_status + '\'' +
+                ", user=" + user +
+                ", sprint=" + sprint +
                 '}';
     }
 }
